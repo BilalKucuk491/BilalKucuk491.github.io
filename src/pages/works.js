5946e21f589d676d7627de5557290d304c656fdb
Bilal Kucuk <bilalkucuk070@gmail.com>
2022-09-15 19:08:15 +0300

    test

--- src/pages/works.js
+++ src/pages/works.js
@@ -4,27 +4,27 @@ import Card from "../components/Card";
 
 export default function Works() {
   return (
-    <div className="container my-12 mx-auto">
-      Merhaba
-      <div className="flex flex-wrap mx-10 mt-10">
-
-       {projectConstants.map((item,index)=>(
-         <div key={index} className={classNames({
-          "w-full md:w-1/3 lg:w-1/4 m-10":true,
-        })}>
-          <Card 
-          projectSourceUrl={item.projectSourceUrl}
-          projectLiveUrl={item.projectLiveUrl} 
-          imageSource={item.projectImageSource}
-          projectName={item.projectName}
-          />
-        </div>
-          
-       ))}
-      </div>
-    </div>
-
-   
-  
+    // <div className="container my-12 mx-auto">
+    //   <div className="flex flex-wrap mx-10 mt-10">
+    //     {projectConstants.map((item, index) => (
+    //       <div
+    //         key={index}
+    //         className={classNames({
+    //           "w-full md:w-1/3 lg:w-1/4 m-10": true,
+    //         })}
+    //       >
+    //         <Card
+    //           projectSourceUrl={item.projectSourceUrl}
+    //           projectLiveUrl={item.projectLiveUrl}
+    //           imageSource={item.projectImageSource}
+    //           projectName={item.projectName}
+    //         />
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
+    <>
+    Merhaba
+    </>
   );
 }
